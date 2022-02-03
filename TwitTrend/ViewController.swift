837b932d374727742073213f5415566df58a4d34
Majd <m3khadra@gmail.com>
2022-02-03 01:45:20 -0500

    project created, storyboard removed

---
+++ TwitTrend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitTrend
+//
+//  Created by Majd Khadra on 2022-02-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
